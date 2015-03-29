50c0a5f3e221af8382ef043b83b9c461b86fcd36
Larry Ng <ng.larry@gmail.com>
2015-03-29 16:36:23 -0400

    bot_id and username aren't always present

--- src/main/scala/com/github/slackey/codecs/types/BotMessage.scala
+++ src/main/scala/com/github/slackey/codecs/types/BotMessage.scala
@@ -2,15 +2,15 @@ package com.github.slackey.codecs.types
 
 object BotMessage {
   def apply(m: Message): BotMessage =
-    BotMessage(m.`type`, m.subtype.get, m.channel, m.bot_id.get, m.username.get, m.text.get, m.ts)
+    BotMessage(m.`type`, m.subtype.get, m.channel, m.text.get, m.bot_id, m.username, m.ts)
 }
 
 case class BotMessage(
   `type`: String,
   subtype: String,
   channel: String,
-  bot_id: String,
-  username: String,
   text: String,
+  username: Option[String],
+  bot_id: Option[String],
   ts: String
 )
