54a07a0d0590e9db126de5a23f2aaff527d12d02
Larry Ng <ng.larry@gmail.com>
2015-03-29 23:46:12 -0400

    use stdlib Random

--- src/main/scala/com/github/slackey/examples/EchoListener.scala
+++ src/main/scala/com/github/slackey/examples/EchoListener.scala
@@ -1,10 +1,11 @@
 package com.github.slackey.examples
 
+import scala.util.Random
+
 import org.json4s._
 
 import com.github.slackey.bot.{BotMessages, RealTimeMessagingListener, SlackState}
 import com.github.slackey.codecs.types.SimpleMessage
-import com.github.slackey.util.randomReplyId
 
 object EchoListener extends RealTimeMessagingListener {
   import BotMessages._
@@ -12,7 +13,7 @@ object EchoListener extends RealTimeMessagingListener {
   override def onSimpleMessage(state: SlackState, message: SimpleMessage, json: JObject): Seq[SendMessage] = {
     val user = state.userById.get(message.user)
     val name = user.fold("<unknown>")(_.name)
-    val replyId = randomReplyId
+    val replyId = Random.nextLong()
     val text = s"$name said: ${message.text}"
     Seq(SendMessage(replyId, message.channel, text))
   }
--- src/main/scala/com/github/slackey/util/package.scala
+++
@@ -1,5 +0,0 @@
-package com.github.slackey
-
-package object util {
-  def randomReplyId = (Math.random() * Long.MaxValue).toLong
-}
